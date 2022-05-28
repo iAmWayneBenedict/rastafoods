98cb3eb4ccf094ff82d9101cad99d36e3c1ecf70
iAmWayne <iraolawayne@gmail.com>
2022-05-28 09:47:53 +0800

    added discover foods

--- src/components/home_components/DiscoverFoods.component.js
+++ src/components/home_components/DiscoverFoods.component.js
@@ -1,7 +1,36 @@
+import Card from "../home_components/discover_foods_components/Card.component";
+
 const DiscoverFoods = () => {
+	const cardData = {
+		burger: {
+			name: "Burgers",
+			src: "img/t-mcdonalds-qpc-deluxe-burger 1.png",
+		},
+		drinks: {
+			name: "Drinks",
+			src: "img/Rectangle 15.png",
+		},
+		fried_chicken: {
+			name: "Fried Chicken",
+			src: "img/Fried Chicken.png",
+		},
+		sisig: {
+			name: "Sisig",
+			src: "img/Sisig.png",
+		},
+		bundle_meals: {
+			name: "Bundle Meals",
+			src: "img/Inasal.png",
+		},
+		halohalo: {
+			name: "Halo Halo",
+			src: "img/halo halo.png",
+		},
+	};
+
 	return (
 		<div className="current flex flex-col mt-[8rem] py-10 relative">
-			<span className="absolute top-0 left-0 w-screen h-full bg-accent z-[-1]"></span>
+			<span className="absolute top-0 left-0 w-screen h-full z-[-1]"></span>
 			<div className="title flex flex-col items-center">
 				<h1 className="font-semibold xl:text-4xl text-4xl leading-snug text-center mb-5">
 					Discover Foods
@@ -10,10 +39,10 @@ const DiscoverFoods = () => {
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 				</p>
 			</div>
-			<div className="flex items-center md:justify-center mt-20 flex-col md:flex-row">
-				{/* {Object.entries(cardData).map(([key, value]) => {
+			<div className="flex items-center md:justify-center mt-20 flex-col md:flex-row flex-wrap">
+				{Object.entries(cardData).map(([key, value]) => {
 					return <Card data={value} key={key} />;
-				})} */}
+				})}
 			</div>
 		</div>
 	);
--- src/components/home_components/discover_foods_components/Card.component.js
+++ src/components/home_components/discover_foods_components/Card.component.js
@@ -1,12 +1,14 @@
-const Card = () => {
+const Card = ({ data }) => {
+	const { name, src } = data;
+
 	return (
 		<>
-			<div className="container">
-				<div className="img-con">
-					<img src="" alt="" />
+			<div className="w-56 h-52 md:w-[17rem] md:h-[15rem] rounded-xl p-8 flex flex-col justify-between hover:shadow-xl hover:shadow-[#ff25251a] m-2 md:m-5 cursor-pointer">
+				<div className="img-con w-2/3 md:w-auto">
+					<img src={src} alt={name} />
 				</div>
 				<div className="txt-con">
-					<h1>Burgers</h1>
+					<h1 className="font-semibold text-xl">{name}</h1>
 				</div>
 			</div>
 		</>
