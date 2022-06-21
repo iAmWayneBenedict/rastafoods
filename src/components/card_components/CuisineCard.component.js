5eec198593059431e44daffef0eba44bd86d19e4
iAmWayne <iraolawayne@gmail.com>
2022-06-21 15:14:48 +0800

    added CuisineCard

--- src/components/card_components/CuisineCard.component.js
+++ src/components/card_components/CuisineCard.component.js
@@ -1,9 +1,16 @@
-const CuisineCard = () => {
+import { Link } from "react-router-dom";
+
+const CuisineCard = ({ data }) => {
+	const { name, src } = data;
+
 	return (
-		<div>
-			<h1>Hello World</h1>
-			<p>1</p>
-		</div>
+		<Link
+			to={"/"}
+			className="border border-gray-300 rounded-md flex items-center py-1 sm:py-2 px-3 pr-5 sm:px-5 gap-3 sm:gap-5 min-w-fit cursor-pointer"
+		>
+			<img src={src} alt="" className="h-[3rem]" />
+			<span className="font-semibold">{name}</span>
+		</Link>
 	);
 };
 
--- src/components/section_components/SectionContainer.js
+++ src/components/section_components/SectionContainer.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import CuisineCard from "../card_components/CuisineCard.component";
 import StoreCard from "../card_components/StoreCard.component";
 
 const SectionContainer = ({ data }) => {
@@ -15,6 +16,33 @@ const SectionContainer = ({ data }) => {
 		else if (section === "foods") setIsFoods(true);
 	}, [section]);
 
+	const cardData = {
+		burger: {
+			name: "Burgers",
+			src: "img/t-mcdonalds-qpc-deluxe-burger 1.png",
+		},
+		drinks: {
+			name: "Drinks",
+			src: "img/Rectangle 15.png",
+		},
+		fried_chicken: {
+			name: "Fried Chicken",
+			src: "img/Fried Chicken.png",
+		},
+		sisig: {
+			name: "Sisig",
+			src: "img/Sisig.png",
+		},
+		bundle_meals: {
+			name: "Bundle Meals",
+			src: "img/Inasal.png",
+		},
+		halohalo: {
+			name: "Halo Halo",
+			src: "img/halo halo.png",
+		},
+	};
+
 	return (
 		<div className="my-20">
 			<h1 className="font-bold text-xl my-10">{title}</h1>
@@ -24,6 +52,14 @@ const SectionContainer = ({ data }) => {
 						if (val < 8) return <StoreCard event={false} key={val} />;
 						return <StoreCard event={true} key={val} />;
 					})}
+
+				{isCuisines && (
+					<div className="flex gap-3 overflow-x-scroll w-full">
+						{Object.entries(cardData).map(([key, value]) => {
+							return <CuisineCard data={value} key={key} />;
+						})}
+					</div>
+				)}
 			</div>
 		</div>
 	);
