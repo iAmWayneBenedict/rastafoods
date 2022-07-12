70b2ce8eaa7d4ba801d864200b7f99393d9654b3
iAmWayne <iraolawayne@gmail.com>
2022-07-12 13:58:29 +0800

    added preloader

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Stores from "./components/Stores";
 import ScrollToTop from "./components/ScrollToTop";
 import useScroll from "./custom_hooks/useScroll";
 import NavBar from "./components/Navbar";
-import Footer from "./components/Footer";
-import CartSection from "./components/card_components/CartSection.component";
+import CartSection from "./components/CartSection";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
@@ -14,23 +13,24 @@ const App = () => {
 	let { scrolledOverHundred, scrolledOverTen } = useScroll();
 
 	return (
-		<div className="App font-sans 2xl:mx-[10rem] lg:mx-[5rem] md:mx-0 px-5 relative">
-			<Router>
-				{/* Navigation bar */}
-				<NavBar hasScrolled={scrolledOverTen} />
-				{/* Scroll To Top */}
-				<ScrollToTop hasScrolled={scrolledOverHundred} />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/stores" element={<Stores />} />
-					<Route path="/stores/:store/:food" element={<Stores />} />
-					<Route path="/cart" element={<CartSection />} />
-					<Route path="/login" element={<Login />}></Route>
-					<Route path="/signup" element={<Signup />}></Route>
-				</Routes>
-				{/* Footer */}
-				<Footer />
-			</Router>
+		<div className="App relative">
+			<div className="font-sans 2xl:mx-[10rem] lg:mx-[5rem] md:mx-0 px-5 ">
+				<Router>
+					{/* Navigation bar */}
+					<NavBar hasScrolled={scrolledOverTen} />
+
+					{/* Scroll To Top */}
+					<ScrollToTop hasScrolled={scrolledOverHundred} />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/stores" element={<Stores />} />
+						<Route path="/stores/:store/:food" element={<Stores />} />
+						<Route path="/cart" element={<CartSection />} />
+						<Route path="/login" element={<Login />}></Route>
+						<Route path="/signup" element={<Signup />}></Route>
+					</Routes>
+				</Router>
+			</div>
 		</div>
 	);
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,30 +6,43 @@ import GetInTouch from "./home_components/GetInTouch.component";
 import HowItWorks from "./home_components/HowItWorks.component";
 import Rider from "./home_components/RIder.component";
 import RowMenu from "./home_components/RowMenu.component";
+import Preloader from "./preloader_component/Preloader.component";
+import usePreloader from "../custom_hooks/usePreloader";
+import Footer from "./Footer";
 
 const Home = () => {
+	let loaderValue = usePreloader();
+
 	return (
 		<div>
-			<div className="2xl:mx-[10rem] lg:mx-[2rem]">
-				{/*  Banner component */}
-				<Banner />
-				{/* About component */}
-				<About />
-			</div>
-			{/* How It Works component */}
-			<HowItWorks />
-			<div className="2xl:mx-[10rem] lg:mx-[2rem]">
-				{/* Row Menu Component */}
-				<RowMenu />
-				{/* Discover Foods Component */}
-				<DiscoverFoods />
-				{/* Business Section Component */}
-				<Business />
-				{/* Rider Section Component */}
-				<Rider />
-				{/* Get In Touch Section Component */}
-				<GetInTouch />
-			</div>
+			<Preloader loaderValue={loaderValue} />
+			{loaderValue === 2 && (
+				<div>
+					<div className="2xl:mx-[10rem] lg:mx-[2rem]">
+						{/*  Banner component */}
+						<Banner />
+						{/* About component */}
+						<About />
+					</div>
+					{/* How It Works component */}
+					<HowItWorks />
+					<div className="2xl:mx-[10rem] lg:mx-[2rem]">
+						{/* Row Menu Component */}
+						<RowMenu />
+						{/* Discover Foods Component */}
+						<DiscoverFoods />
+						{/* Business Section Component */}
+						<Business />
+						{/* Rider Section Component */}
+						<Rider />
+						{/* Get In Touch Section Component */}
+						<GetInTouch />
+					</div>
+
+					{/* Footer */}
+					<Footer />
+				</div>
+			)}
 		</div>
 	);
 };
