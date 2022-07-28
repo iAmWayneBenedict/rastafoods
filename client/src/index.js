38217ac168281263053fd38c2a2d5ecf51effbd5
iAmWayne <iraolawayne@gmail.com>
2022-07-28 19:34:29 +0800

    migrated to react 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const rootContainer = ReactDOM.createRoot(container);
+rootContainer.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
